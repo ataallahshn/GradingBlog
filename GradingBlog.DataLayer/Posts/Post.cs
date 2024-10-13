aa7f7c0d7d058b2ba6944aa193fa5464308984c7
a.shojaeinassab <a.shojaeinassab@alibaba.ir>
2024-10-13 12:06:34 +0330

    feat: add post unit test

--- GradingBlog.DataLayer/Posts/Post.cs
+++ GradingBlog.DataLayer/Posts/Post.cs
@@ -7,8 +7,11 @@ public sealed class Post : Entity<long>
 {
     public Post(string title, string content)
     {
-        Guard.Against.NullOrWhiteSpace(title, new Exception("لطفا عنوان محتوا وارد"));
-        Guard.Against.NullOrWhiteSpace(title, new Exception("لطفا بدنه محتوا وارد"));
+        Guard.Against.NullOrWhiteSpace(title, new Exception("Title Is Null"));
+
+        Guard.Against.NullOrWhiteSpace(content, new Exception("Content Is Null"));
+
+        Guard.Against.GreaterThan(title.Length, 100, new Exception("max title length is 100"));
 
         Title = title;
         Content = content;
